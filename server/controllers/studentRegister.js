fbf68af351791ce9a1e5f6a7420dd72ae0c82726
Aryan <aryan04@somaiya.edu>
2022-01-21 23:23:08 +0530

    bugs

--- server/controllers/studentRegister.js
+++ server/controllers/studentRegister.js
@@ -6,7 +6,7 @@ const Student = require("../models/Student.js");
 
 exports.registerStudent = async (req, res, next) => {
   const { name, email, roll_number, img } = req.body;
-  const x = encodedArray(img);
+
   try {
     const user = await Student.create({
       name,
@@ -21,26 +21,8 @@ exports.registerStudent = async (req, res, next) => {
   }
 };
 
-const encodedArray = (image) => {
-  let Ec;
-  console.log(image);
-  let getEncodedArray = spawn("python", ["./test.py", image]);
-
-  getEncodedArray.stdout.on("data", (data) => {
-    console.log(data);
-    Ec = data;
-  });
-  getEncodedArray.stderr.on("data", (data) => {
-    console.log(toString(data));
-  });
-
-  getEncodedArray.on("close", (code) => {
-    console.log(code);
-  });
-  return Ec;
-};
 const sendToken = (user, statusCode, res) => {
-  const token = "gaandmara";
+  const token = "oof";
   res.status(statusCode).json({
     success: true,
     token,
