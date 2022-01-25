e4b86a2cd488894b1ede33fc97b948686082cc3e
Shruti Shinde <sss22@somaiya.edu>
2022-01-25 23:46:13 +0530

    added upload image functionality

--- server/server.js
+++ server/server.js
@@ -1,17 +1,26 @@
 require("dotenv").config({ path: ".env" });
 const express = require("express");
+const cors = require("cors");
+const studentRoute = require("./routes/studentRegister");
+const privateRoute = require("./routes/private");
+const authRoute = require("./routes/auth");
 const connectDB = require("./config/db");
 const errorHandler = require("./middleware/error");
 //Connect DB
 connectDB();
 const app = express();
+app.use(cors());
 app.use(express.json({ limit: "50mb" }));
-app.use("/", require("./routes/auth"));
-app.use("/student", require("./routes/studentRegister"));
-app.use("/private", require("./routes/private"));
+app.use("/", authRoute);
+console.log("connected");
+app.use("/student", studentRoute);
+app.use("/private", privateRoute);
 app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
-
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({ error: err.message, data: data });
+});
 const server = app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
