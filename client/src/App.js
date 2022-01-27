332d4bc5e661a4aa90dbb0529d8be6f01d9f920c
Shruti Shinde <sss22@somaiya.edu>
2022-01-27 18:24:44 +0530

    attendance page and protected routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,45 @@
-import "./App.css";
+import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import AdminPage from "./pages/AdminPage";
 import TeacherPage from "./pages/TeacherPage";
+import AttendancePage from "./pages/AttendancePage";
 import LoginPage from "./pages/LoginPage";
 function App() {
   const role = localStorage.getItem("role");
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route
-          path="/teacher"
-          element={role === "teacher" ? <TeacherPage /> : <Navigate to="/" />}
-        />
-        <Route path="/admin" element={<AdminPage />} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route
+        path="/teacher"
+        element={(() => {
+          if (role === "teacher") {
+            return <TeacherPage />;
+          } else {
+            return <Navigate to="/" />;
+          }
+        })()}
+      />
+      <Route
+        path="/admin"
+        element={(() => {
+          if (role === "admin") {
+            return <AdminPage />;
+          } else {
+            return <Navigate to="/" />;
+          }
+        })()}
+      />
+      <Route
+        path="/attendance-session"
+        element={(() => {
+          if (role === "teacher") {
+            return <AttendancePage />;
+          } else {
+            return <Navigate to="/" />;
+          }
+        })()}
+      />
+    </Routes>
   );
 }
 
